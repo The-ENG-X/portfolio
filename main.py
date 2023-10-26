f4e1453d16b3a934cf267bd93bcde5c84c256369
My-Levi <eng.ahmadalebrahim@gmail.com>
2023-10-26 17:08:45 +0200

    Updated portfolio HTML&Python

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+name = "Ahmad Al Ibrahim"
+skills = ["AWS", "JavaScript", "Python" ]
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request, "name": name, "skills": skills})
